8ead013907b0a541ff9f85b97dcd28657d29fc5e
JR Ladd <johnrobertladd@gmail.com>
2017-07-23 22:11:45 -0400

    fixed long s

--- src/expand_names.py
+++ src/expand_names.py
@@ -6,19 +6,41 @@ from collections import Counter
 with open('data/1640s_reg_weighted_curated.tsv', 'r') as csvfile:
     reader = csv.reader(csvfile, delimiter='\t')
     data = [r for r in reader]
+    data = data[1:]
     cleaned = []
     for d in data:
         if len(d) != 4:
             d.append('f')
-        d.append(d[1].title())
+        d.append(d[1].title().replace('\u017f', 's'))
         cleaned.append(d)
 
-#for c in cleaned:
-    #print(c)
-
-cleaned_edges = [(c[0], c[-1]) for c in cleaned]
+cleaned_edges = [(c[0], c[2], c[3], c[4]) for c in cleaned]
 counted = Counter(cleaned_edges)
 
+aggregated = []
 for k,v in counted.items():
+    k = list(k)
     if v > 1:
-        print(k,v)
+        print(k, v)
+        k[1] = '2'
+    aggregated.append(k)
+
+#aggregated_fixed = []
+#for a in aggregated:
+#    a[-1] = a[-1].replace('\u017f', 's')
+#    aggregated_fixed.append(a)
+
+
+christ = ['Jesus', 'Iesus', 'Christ ', 'Christs']
+standardized = []
+for a in aggregated:
+    if any(c in a[-1] for c in christ) or a[-1].endswith('Christ'):
+        a.append('Jesus Christ')
+    standardized.append(a)
+
+#for s in standardized:
+    #print(s)
+
+with open('data/1640s_standardized.tsv', 'w') as newcsv:
+    writer = csv.writer(newcsv, delimiter='\t')
+    writer.writerows(standardized)
