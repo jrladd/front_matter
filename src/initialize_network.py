950214e8026cbb5616438d13ca41687abf200980
JR Ladd <johnrobertladd@gmail.com>
2017-07-28 01:26:21 -0400

    final data analyses and viz fixes

--- src/initialize_network.py
+++ src/initialize_network.py
@@ -1,10 +1,14 @@
 #! /usr/bin/env python3
 
-import csv, json
+import csv, json, sqlite3
 from collections import Counter
 from operator import itemgetter
 import networkx as nx
 from networkx.algorithms import bipartite
+
+conn = sqlite3.connect("data/eebo_tcp_metadata.sqlite")
+
+c = conn.cursor()
 
 i_to_j = ['Iohn', 'Iames']
 with open("data/1640s_expanded_curated_openrefine.tsv", 'r') as csvfile:
@@ -57,15 +61,39 @@ betw=bipartite.betweenness_centrality(B,text_nodes)
 close=bipartite.closeness_centrality(B,text_nodes,normalized=True)
 
 def get_rank(dictionary):
-    sorted_dict = sorted(dictionary.items(), key=itemgetter(1), reverse=True)
-    rank = {s[0]:sorted_dict.index(s)+1 for s in sorted_dict}
-    return rank
+    dictionary0 = {k:v for k,v in dictionary.items() if k in text_nodes}
+    dictionary1 = {k:v for k,v in dictionary.items() if k not in text_nodes}
+    sorted_dict0 = sorted(dictionary0.items(), key=itemgetter(1), reverse=True)
+    sorted_dict1 = sorted(dictionary1.items(), key=itemgetter(1), reverse=True)
+    print(sorted_dict1[:10])
+    rank0 = {s[0]:sorted_dict0.index(s)+1 for s in sorted_dict0}
+    rank1 = {s[0]:sorted_dict1.index(s)+1 for s in sorted_dict1}
+    return dict(list(rank0.items())+list(rank1.items()))
 
 degree_rank = get_rank(dict(list(deg_people.items())+list(deg_texts.items())))
 betw_rank = get_rank(betw)
 close_rank = get_rank(close)
 components = nx.connected_components(B)
 largest_component = max(components, key=len)
+
+title = {}
+author = {}
+date = {}
+for n in B.nodes():
+    if n in text_nodes:
+        t = (n,)
+        c.execute("SELECT title,author,date FROM metadata WHERE key = ?;", t)
+        result = c.fetchone()
+        if len(result[0]) > 50:
+            title[n] = result[0][:50]+'...'
+        else:
+            title[n] = result[0]
+        author[n] = result[1]
+        date[n] = int(result[2])
+    else:
+        title[n] = None
+        author[n] = None
+        date[n] = None
 
 # Create a "subgraph" of just the largest component
 # Then calculate the diameter of the subgraph, just like you did with density.
@@ -79,6 +107,9 @@ nx.set_node_attributes(B, 'closeness', close)
 nx.set_node_attributes(B, 'deg_rank', degree_rank)
 nx.set_node_attributes(B, 'betw_rank', betw_rank)
 nx.set_node_attributes(B, 'close_rank', close_rank)
+nx.set_node_attributes(B, 'title', title)
+nx.set_node_attributes(B, 'author', author)
+nx.set_node_attributes(B, 'date', date)
 #SB.remove_node('Jesus Christ')
 
 # Create a dictionary for the JSON needed by D3.
@@ -91,6 +122,9 @@ new_data = dict(
         deg_rank=SB.node[n]['deg_rank'],
         betw_rank=SB.node[n]['betw_rank'],
         close_rank=SB.node[n]['close_rank'],
+        title=SB.node[n]['title'],
+        author=SB.node[n]['author'],
+        date=SB.node[n]['date'],
         bipartite=SB.node[n]['bipartite']) for n in SB.nodes()],
     links=[dict(source=e[0], target=e[1], weight=e[2]) for e in SB.edges(data='weight')])
 
