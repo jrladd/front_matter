2ce6b89e2b184fbbb45377e9a60a0b7f75f8920b
JR Ladd <johnrobertladd@gmail.com>
2017-09-27 21:11:22 -0400

    graph degree and betweenness

--- src/initialize_network.py
+++ src/initialize_network.py
@@ -1,10 +1,11 @@
 #! /usr/bin/env python3
 
-import csv, json, sqlite3
+import csv, json, sqlite3, numpy
 from collections import Counter
 from operator import itemgetter
 import networkx as nx
 from networkx.algorithms import bipartite
+import matplotlib.pyplot as plt
 
 conn = sqlite3.connect("data/eebo_tcp_metadata.sqlite")
 
@@ -60,74 +61,101 @@ deg_people,deg_texts=bipartite.degrees(B,text_nodes,'weight')
 betw=bipartite.betweenness_centrality(B,text_nodes)
 close=bipartite.closeness_centrality(B,text_nodes,normalized=True)
 
-def get_rank(dictionary):
-    dictionary0 = {k:v for k,v in dictionary.items() if k in text_nodes}
-    dictionary1 = {k:v for k,v in dictionary.items() if k not in text_nodes}
-    sorted_dict0 = sorted(dictionary0.items(), key=itemgetter(1), reverse=True)
-    sorted_dict1 = sorted(dictionary1.items(), key=itemgetter(1), reverse=True)
-    print(sorted_dict1[:10])
-    rank0 = {s[0]:sorted_dict0.index(s)+1 for s in sorted_dict0}
-    rank1 = {s[0]:sorted_dict1.index(s)+1 for s in sorted_dict1}
-    return dict(list(rank0.items())+list(rank1.items()))
-
-degree_rank = get_rank(dict(list(deg_people.items())+list(deg_texts.items())))
-betw_rank = get_rank(betw)
-close_rank = get_rank(close)
-components = nx.connected_components(B)
-largest_component = max(components, key=len)
-
-title = {}
-author = {}
-date = {}
-for n in B.nodes():
-    if n in text_nodes:
-        t = (n,)
-        c.execute("SELECT title,author,date FROM metadata WHERE key = ?;", t)
-        result = c.fetchone()
-        if len(result[0]) > 50:
-            title[n] = result[0][:50]+'...'
-        else:
-            title[n] = result[0]
-        author[n] = result[1]
-        date[n] = int(result[2])
-    else:
-        title[n] = None
-        author[n] = None
-        date[n] = None
-
-# Create a "subgraph" of just the largest component
-# Then calculate the diameter of the subgraph, just like you did with density.
-# 
-
-SB = B.subgraph(largest_component)
-SB.remove_nodes_from(node for node, degree in deg_people.items() if degree <= 1)
-nx.set_node_attributes(B, 'degree', dict(list(deg_people.items())+list(deg_texts.items())))
-nx.set_node_attributes(B, 'betweenness', betw)
-nx.set_node_attributes(B, 'closeness', close)
-nx.set_node_attributes(B, 'deg_rank', degree_rank)
-nx.set_node_attributes(B, 'betw_rank', betw_rank)
-nx.set_node_attributes(B, 'close_rank', close_rank)
-nx.set_node_attributes(B, 'title', title)
-nx.set_node_attributes(B, 'author', author)
-nx.set_node_attributes(B, 'date', date)
+# def get_rank(dictionary):
+#     dictionary0 = {k:v for k,v in dictionary.items() if k in text_nodes}
+#     dictionary1 = {k:v for k,v in dictionary.items() if k not in text_nodes}
+#     sorted_dict0 = sorted(dictionary0.items(), key=itemgetter(1), reverse=True)
+#     sorted_dict1 = sorted(dictionary1.items(), key=itemgetter(1), reverse=True)
+#     print(sorted_dict1[:10])
+#     rank0 = {s[0]:sorted_dict0.index(s)+1 for s in sorted_dict0}
+#     rank1 = {s[0]:sorted_dict1.index(s)+1 for s in sorted_dict1}
+#     return dict(list(rank0.items())+list(rank1.items()))
+#
+# degree_rank = get_rank(dict(list(deg_people.items())+list(deg_texts.items())))
+# betw_rank = get_rank(betw)
+# close_rank = get_rank(close)
+# components = nx.connected_components(B)
+# largest_component = max(components, key=len)
+#
+# title = {}
+# author = {}
+# date = {}
+# for n in B.nodes():
+#     if n in text_nodes:
+#         t = (n,)
+#         c.execute("SELECT title,author,date FROM metadata WHERE key = ?;", t)
+#         result = c.fetchone()
+#         if len(result[0]) > 50:
+#             title[n] = result[0][:50]+'...'
+#         else:
+#             title[n] = result[0]
+#         author[n] = result[1]
+#         date[n] = int(result[2])
+#     else:
+#         title[n] = None
+#         author[n] = None
+#         date[n] = None
+#
+# # Create a "subgraph" of just the largest component
+# # Then calculate the diameter of the subgraph, just like you did with density.
+# #
+#
+# SB = B.subgraph(largest_component)
+# SB.remove_nodes_from(node for node, degree in deg_people.items() if degree <= 1)
+# nx.set_node_attributes(B, 'degree', dict(list(deg_people.items())+list(deg_texts.items())))
+# nx.set_node_attributes(B, 'betweenness', betw)
+# nx.set_node_attributes(B, 'closeness', close)
+# nx.set_node_attributes(B, 'deg_rank', degree_rank)
+# nx.set_node_attributes(B, 'betw_rank', betw_rank)
+# nx.set_node_attributes(B, 'close_rank', close_rank)
+# nx.set_node_attributes(B, 'title', title)
+# nx.set_node_attributes(B, 'author', author)
+# nx.set_node_attributes(B, 'date', date)
 #SB.remove_node('Jesus Christ')
 
 # Create a dictionary for the JSON needed by D3.
-new_data = dict(
-    nodes=[dict(
-        id=n,
-        degree=SB.node[n]['degree'],
-        betweenness=SB.node[n]['betweenness'],
-        closeness=SB.node[n]['closeness'],
-        deg_rank=SB.node[n]['deg_rank'],
-        betw_rank=SB.node[n]['betw_rank'],
-        close_rank=SB.node[n]['close_rank'],
-        title=SB.node[n]['title'],
-        author=SB.node[n]['author'],
-        date=SB.node[n]['date'],
-        bipartite=SB.node[n]['bipartite']) for n in SB.nodes()],
-    links=[dict(source=e[0], target=e[1], weight=e[2]) for e in SB.edges(data='weight')])
+# new_data = dict(
+#     nodes=[dict(
+#         id=n,
+#         degree=SB.node[n]['degree'],
+#         betweenness=SB.node[n]['betweenness'],
+#         closeness=SB.node[n]['closeness'],
+#         deg_rank=SB.node[n]['deg_rank'],
+#         betw_rank=SB.node[n]['betw_rank'],
+#         close_rank=SB.node[n]['close_rank'],
+#         title=SB.node[n]['title'],
+#         author=SB.node[n]['author'],
+#         date=SB.node[n]['date'],
+#         bipartite=SB.node[n]['bipartite']) for n in SB.nodes()],
+#     links=[dict(source=e[0], target=e[1], weight=e[2]) for e in SB.edges(data='weight')])
+#
+# # Output json of the graph.
+# with open('viz/1640s_dedications.json', 'w') as output:
+#     json.dump(new_data, output, sort_keys=True, indent=4, separators=(',',':'))
 
-# Output json of the graph.
-with open('viz/1640s_dedications.json', 'w') as output:
-    json.dump(new_data, output, sort_keys=True, indent=4, separators=(',',':'))
+# plt.scatter(x2,y2,c='.75',alpha=.25)
+# labels = list(deg_people.keys())
+people_nodes = [n for n,d in B.nodes(data=True) if d['bipartite'] == 1]
+x = [deg_people[l] for l in people_nodes]
+y = [betw[l] for l in people_nodes]
+
+plt.scatter(x,y,c='r',alpha=1)
+
+for label, a, b in zip(people_nodes,x,y):
+    if a > 4:
+        plt.annotate(
+            label,
+            xy=(a,b), xytext=(-1,1),
+            textcoords='offset points', ha='right', va='bottom'
+            # bbox=dict(boxstyle='round,pad=0.5', fc='yellow', alpha=0.5),
+            # arrowprops=dict(arrowstyle = '->', connectionstyle='arc3,rad=0')
+        )
+
+coefficients = numpy.polyfit(x,y,1)
+polynomial = numpy.poly1d(coefficients)
+ys = polynomial(x)
+plt.plot(x, ys, alpha=.5)
+
+# plt.xscale('log')
+# plt.yscale('log')
+plt.show()
