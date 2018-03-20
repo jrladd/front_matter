d3b1d9bc0e7c03b6185e166f507ee5706d0fa014
JR Ladd <johnrobertladd@gmail.com>
2018-03-20 13:34:46 -0500

    use groupby instead of for loop

--- src/process_ma.py
+++ src/process_ma.py
@@ -10,16 +10,6 @@ for c in csvfiles[:10]:
     with open(c, 'r') as csvfile:
         reader = csv.reader(csvfile, delimiter='\t')
         reader = [r for r in reader]
-        # for i,r in enumerate(reader):
-        #     if 'np' in r[2] and '.' not in r[0]:
-        #         # print(r[0],r[2])
-        #         try:
-        #             if 'np' in reader[i+1][2]: #and '.' not in r[i+1][0]:
-        #                 print(r[0]+' '+reader[i+1][0])
-        #             # else:
-        #             #     print(r[0])
-        #         except IndexError:
-        #             pass
         for k,g in groupby(reader, key=itemgetter(2)):
             group = [x for x in g]
             if 'np' in k and len(group) > 1:
