a8bf1fe17e0e7ed8630f450cabcff8056b518341
JR Ladd <johnrobertladd@gmail.com>
2020-07-13 16:04:17 -0500

    use double metaphone

--- src/epnames.py
+++ src/epnames.py
@@ -4,23 +4,25 @@ from lxml import etree
 import glob, csv, sys, pycorpora, re
 from itertools import groupby
 from collections import Counter
+from metaphone import doublemetaphone
 
 stopwords = pycorpora.geography.nationalities["nationalities"]
 stopwords.extend(pycorpora.geography.countries["countries"])
 stopwords.extend(pycorpora.geography.english_towns_cities["towns"])
 stopwords.extend(pycorpora.geography.english_towns_cities["cities"])
-stopwords.extend(["London", "Westminster", "England", "Scotland", "Thames", "Christian", "Christians", "Christian's", "Candlemas", "Christmas", "Jews", "Jew", "Israelites", "Egyptians", "Protestant", "Protestants", "Catholic", "Catholics", "Presbyterian", "Presbyterians", "Puritan", "Puritans", "Papist", "Papists", "Evangelical", "Version", "Hospital", "Surrey", "Barnstable", "Devon", "Wales", "Elysians", "Martyrs", "College", "Turks", "Canaanites", "Hampshire", "Huns", "Jesuits", "Enchiridion", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December", "Trinity", "Athens", "Anabaptists", "Philistines", "Christendom", "Odyssey", "Iliad", "Europe"])
+stopwords.extend(["London", "Westminster", "England", "Scotland", "Thames", "Candlemas", "Christmas", "Jews", "Jew", "Israelites", "Egyptians", "Protestant", "Protestants", "Catholic", "Catholics", "Presbyterian", "Puritan", "Papist", "Evangelical", "Version", "Hospital", "Surrey", "Barnstable", "Devon", "Wales", "College", "Turk", "Canaanite", "Hampshire", "Huns", "Jesuit", "Enchiridion", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December", "Trinity", "Athens", "Anabaptist", "Philistine", "Christendom", "Odyssey", "Iliad", "Europe", "Christianity", "Christian", "Rome", "Britain"])
 
 prefixes = pycorpora.humans.prefixes["prefixes"]
-prefixes.extend(["Mris", "Mris.", "Sr", "Sr.", "Prophet", "Master", "Alderman", "Cap.", "Captain", "Vicount", "Viscount", "Vicounte", "Viscounte", "Apostle", "Monarch", "K.", "Q.", "Saint", "St", "S.", "St.", "Mayor", "Mayore", "Maior", "Maiore"])
+prefixes.extend(["M.", "Mris", "Mris.", "Sr", "Sr.", "Prophet", "Master", "Alderman", "Cap.", "Captain", "Vicount", "Viscount", "Vicounte", "Viscounte", "Apostle", "Monarch", "K.", "Q.", "Saint", "St", "S.", "St.", "Mayor", "Mayore", "Maior", "Maiore"])
 
 first_names = pycorpora.humans.firstNames["firstNames"]
 first_names.extend(["Will"])
 
-ordinals = ["First", "Second", "Third", "Fourth", "Fifth", "Sixth", "Seventh", "Eighth", "Ninth", "Tenth"]
+ordinals = ["First", "Second", "Third", "Fourth", "Fifth", "Sixth", "Seventh", "Eighth", "Ninth", "Tenth", "Eleventh", "Twelfth", "Thirteenth", "Fourteenth", "Fifteenth"]
+ofs = ["of", "de", "du"]
 
 def is_proper_noun(tag):
-    if tag != None and "nn" in tag.get('pos', '') and "j" not in tag.get('pos', ''):
+    if tag != None and (tag.get('pos', '') == 'nn1' or tag.get('pos', '') == 'nng1'):
         return True
     else:
         return False
@@ -35,7 +37,8 @@ def is_personal_of(tag):
     if tag != None:
         next_tag = tag.getnext()
         previous_tag = tag.getprevious()
-        if tag.get('reg', tag.text) == "of" and (is_proper_noun(next_tag) or is_personal_title(next_tag) or upper_tag(next_tag) or title_tag(next_tag)) and (is_proper_noun(previous_tag) or is_personal_title(previous_tag)):
+        reg = tag.get('reg', tag.text)
+        if reg != None and reg.lower() in ofs and (is_proper_noun(next_tag) or is_personal_title(next_tag) or upper_tag(next_tag) or title_tag(next_tag)) and (is_proper_noun(previous_tag) or is_personal_title(previous_tag)):
             return True
         else:
             return False
@@ -119,7 +122,7 @@ def title_tag(tag):
 
 def is_royal_numeral(tag):
     if tag != None:
-        if tag.text != None and (re.fullmatch(r"[IV]+\.?|[1-9](r?d\.?|th\.?)", tag.get('reg', tag.text)) or tag.get('reg', tag.text).title() in ordinals):
+        if tag.text != None and (re.fullmatch(r"[IVX]+\.?|[1-9](r?d\.?|th\.?)", tag.get('reg', tag.text)) or tag.get('reg', tag.text).title() in ordinals):
             return True
         else:
             return False
@@ -171,14 +174,19 @@ def is_name(tag):
         return False
 
 def fingerprint(name):
-    name = name.lower()
+    #name = name.lower()
     for p in prefixes:
-        name = re.sub(f"\b{p.lower()}\b", "", name)
+        patt = f"\\b{p.lower()}\\b"
+        name = re.sub(patt, "", name.lower())
+    name = re.sub(r"[\.\?,!;:\(\)]", "", name)
     if " " in name:
-        namelist = [name.split()[0][0]]
-        namelist.extend(name.split()[1:])
-        name = "".join(sorted(namelist))
-    return name
+#        namelist = [name.split()[0][0]]
+#        namelist.extend(name.split()[1:])
+#        name = "".join(sorted(namelist))
+        name = "".join(sorted(name.split()))
+    dm = doublemetaphone(name)
+    no_vowels = re.sub(r"[aeiouy]", "", name.lower())
+    return (dm[0], dm[1], no_vowels)
 
 if __name__ == "__main__":
 
@@ -186,12 +194,19 @@ if __name__ == "__main__":
     files = glob.glob("/home/data/eebotcp/texts/*/*.xml")
     parser = etree.XMLParser(collect_ids=False)
     names = []
+#    pos = []
     for f in files[:1000]:
         tree = etree.parse(f, parser)
         xml = tree.getroot()
         dedications = xml.findall(".//tei:*[@type='dedication']", namespaces=nsmap)
         for dedication in dedications:
             tokens = dedication.xpath(".//tei:w|.//tei:pc", namespaces=nsmap)
+#            pos.extend([(t.get('reg', t.text), t.get('pos', '')) for t in tokens if "nn" in t.get('pos', '')])
+#    print(Counter(pos))
+#    pos = sorted(pos, key = lambda x:x[1])
+#    for k,g in groupby(pos, key = lambda x:x[1]):
+#        list_g = list(g)
+#        print(k, len(list_g), list_g)
             tested = [(t.get('reg', t.text), is_name(t)) for t in tokens]
             for k,g in groupby(tested, key=lambda x:x[1]):
                 if k == True:
